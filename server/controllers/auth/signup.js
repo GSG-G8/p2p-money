be4077edc94cf0fd25e10093a6cf0e4ace3d18a3
ha77an <ha77an19@gmail.com>
2020-06-22 02:49:28 +0300

    used async await and try catch

--- server/controllers/auth/signup.js
+++ server/controllers/auth/signup.js
@@ -4,7 +4,7 @@ const { sign } = require('jsonwebtoken');
 const { client } = require('../../database/models');
 const signUpValidation = require('../../utils/validations/signUpValidation');
 
-const signUp = (req, res, next) => {
+const signUp = async (req, res, next) => {
   const {
     fullName,
     email,
@@ -15,127 +15,79 @@ const signUp = (req, res, next) => {
     mainBankAccount,
   } = req.body;
 
+  const addClient = async (value) => {
+    const checkClient = await client.find(value);
+    if (checkClient.length !== 0) {
+      res.status(400).json({
+        status: 400,
+        message: `${
+          value.email || value.mobileNumber
+        } is already exists, please sign-in`,
+      });
+    } else {
+      const hashedPassword = await hash(password, 10);
+      const { _id } = await client.create({
+        fullName,
+        email: value.email,
+        mobileNumber: value.mobileNumber,
+        password: hashedPassword,
+        mainBankName,
+        mainBankAccount,
+        bankAccounts: [
+          {
+            bankName: mainBankName,
+            accountNumber: mainBankAccount,
+            balance: [{ type: 'USD', total: 1000 }],
+          },
+        ],
+        mainBalance: [{ type: 'USD', total: 1000 }],
+      });
+      const userToken = { clientId: _id };
+      const cookie = sign(userToken, process.env.SECRET_KEY);
+      res.cookie('client', cookie).json({
+        status: 'successfully',
+        role: 'client',
+        _id,
+      });
+    }
+  };
+
   let preferredContact = '';
   if (email) {
     preferredContact = 'email';
   } else {
     preferredContact = 'mobile';
   }
-  signUpValidation({
-    fullName,
-    email,
-    mobileNumber,
-    password,
-    passwordConfirmation,
-    preferredContact,
-    mainBankName,
-    mainBankAccount,
-  })
-    .then(() => {
-      client
-        .findOne({
-          bankAccounts: {
-            $elemMatch: {
-              accountNumber: mainBankAccount,
-            },
-          },
-        })
-        .then((data) => {
-          if (data !== null) {
-            res.status(400).json({
-              status: 400,
-              message: `Bank account [${mainBankAccount}] is already exists, please sign-in`,
-            });
-          } else if (email) {
-            client.find({ email }).then((Data) => {
-              if (Data.length !== 0) {
-                res.status(400).json({
-                  status: 400,
-                  message: `${email} is already exists, please sign-in`,
-                });
-              } else {
-                hash(password, 10).then((hashedPassword) => {
-                  client
-                    .create({
-                      fullName,
-                      email,
-                      password: hashedPassword,
-                      mainBankName,
-                      mainBankAccount,
-                      bankAccounts: [
-                        {
-                          bankName: mainBankName,
-                          accountNumber: mainBankAccount,
-                          balance: [{ type: 'USD', total: 1000 }],
-                        },
-                      ],
-                      mainBalance: [{ type: 'USD', total: 1000 }],
-                    })
-                    .then(() =>
-                      client.findOne({ email }).then((result) => {
-                        const userToken = { clientId: result.id };
-                        const cookie = sign(userToken, process.env.SECRET_KEY);
-                        res.cookie('client', cookie).json({
-                          status: 'successfully',
-                          role: 'client',
-                          result: {
-                            email,
-                            fullName: `${result.fullName}`,
-                          },
-                        });
-                      })
-                    )
-                    .catch((error) => res.send(error));
-                });
-              }
-            });
-          } else {
-            client.find({ mobileNumber }).then((Data) => {
-              if (Data.length !== 0) {
-                res.status(400).json({
-                  status: 400,
-                  message: `${mobileNumber} is already exists, please sign-in`,
-                });
-              } else {
-                hash(password, 10).then((hashedPassword) => {
-                  client
-                    .create({
-                      fullName,
-                      mobileNumber,
-                      password: hashedPassword,
-                      mainBankName,
-                      mainBankAccount,
-                      bankAccounts: [
-                        {
-                          bankName: mainBankName,
-                          accountNumber: mainBankAccount,
-                          balance: [{ type: 'USD', total: 1000 }],
-                        },
-                      ],
-                      mainBalance: [{ type: 'USD', total: 1000 }],
-                    })
-                    .then(() => {
-                      client.findOne({ mobileNumber }).then((result) => {
-                        const userToken = { clientId: result.id };
-                        const cookie = sign(userToken, process.env.SECRET_KEY);
-                        res.cookie('client', cookie).json({
-                          status: 'successfully',
-                          role: 'client',
-                          result: {
-                            mobileNumber,
-                            fullName: `${result.fullName}`,
-                          },
-                        });
-                      });
-                    })
-                    .catch((error) => res.send(error));
-                });
-              }
-            });
-          }
-        });
-    })
-    .catch((err) => res.send(err));
+  try {
+    await signUpValidation({
+      fullName,
+      email,
+      mobileNumber,
+      password,
+      passwordConfirmation,
+      preferredContact,
+      mainBankName,
+      mainBankAccount,
+    });
+    const checkBankAccount = await client.findOne({
+      bankAccounts: {
+        $elemMatch: {
+          accountNumber: mainBankAccount,
+        },
+      },
+    });
+    if (checkBankAccount !== null) {
+      res.status(400).json({
+        status: 400,
+        message: `Bank account [${mainBankAccount}] is already exists, please sign-in`,
+      });
+    } else if (email) {
+      addClient({ email });
+    } else {
+      addClient({ mobileNumber });
+    }
+  } catch (err) {
+    res.send(err);
+  }
 };
-
 module.exports = signUp;
