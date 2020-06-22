5c9db8ce4cec2c66aa7699519b33cc81b50fe29a
ha77an <ha77an19@gmail.com>
2020-06-22 21:15:35 +0300

    added middleWare to check client and send client id

---
+++ server/controllers/middlewares/index.js
@@ -0,0 +1,24 @@
+const { verify } = require('jsonwebtoken');
+
+require('env2')('config.env');
+
+const isClient = (req, res, next) => {
+  try {
+    verify(req.cookies.client, process.env.SECRET_KEY, (err, token) => {
+      if (err) {
+        res.status(401).send({
+          statusCode: 401,
+          auth: false,
+          message: 'you are Unauthorized',
+        });
+      } else {
+        const { clientId } = token;
+        res.clientId = clientId;
+        next();
+      }
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+module.exports = isClient;
