5d301818abe71d618e2e180b48d84be2d68ababd
ahmadysalah <a.ahmad.salah@gmail.com>
2020-06-22 18:58:29 +0300

    change raute and add end point and change res.id #19

--- server/controllers/routes/client/getClintData.js
+++ server/controllers/routes/client/getClintData.js
@@ -2,7 +2,7 @@ const client = require('../../../database/models/client');
 
 const getClientById = async (req, res) => {
   try {
-    const clintData = await client.findById(req.clientId);
+    const clintData = await client.findById(res.clientId);
     res.status(200).json({ message: 'Successfully', clintData });
   } catch (error) {
     res.status(400).json({ message: "Client doesn't exist" });
--- server/router/client.js
+++ server/router/client.js
@@ -4,6 +4,6 @@ const clientRouter = express.Router();
 
 const { getClintData } = require('../controllers/routes/client');
 
-clientRouter.get(getClintData);
+clientRouter.get('/', getClintData);
 
 module.exports = clientRouter;
