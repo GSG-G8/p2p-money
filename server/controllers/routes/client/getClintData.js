f349db6360bc776fae7b2fd573dbddb3262346bc
ahmadysalah <a.ahmad.salah@gmail.com>
2020-06-22 13:54:45 +0300

    change the id for client to get from middleware #19

--- server/controllers/routes/client/getClintData.js
+++ server/controllers/routes/client/getClintData.js
@@ -2,7 +2,7 @@ const client = require('../../../database/models/client');
 
 const getClientById = async (req, res) => {
   try {
-    const clintData = await client.findById(req.params.id);
+    const clintData = await client.findById(req.clientId);
     res.status(200).json({ message: 'Successfully', clintData });
   } catch (error) {
     res.status(400).json({ message: "Client doesn't exist" });
--- server/router/client.js
+++ server/router/client.js
@@ -4,6 +4,6 @@ const clientRouter = express.Router();
 
 const { getClintData } = require('../controllers/routes/client');
 
-clientRouter.get('/:id', getClintData);
+clientRouter.get(getClintData);
 
 module.exports = clientRouter;
