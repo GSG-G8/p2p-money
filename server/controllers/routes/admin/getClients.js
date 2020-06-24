dabdadaffa7d59ab2715b6f8a95141a0cb548ef5
Ali <ali.dahdouh@gmail.com>
2020-06-24 21:26:10 +0300

    added route tests relates #4

--- server/controllers/routes/admin/getClients.js
+++ server/controllers/routes/admin/getClients.js
@@ -3,9 +3,20 @@ const Client = require('../../../database/models/client');
 const getClients = async (req, res) => {
   try {
     const Clients = await Client.find({});
-    res.status(200).json({ status: 'Successfully', data: Clients });
+    if (!Clients.length) {
+      res.status(200).json({
+        status: 'Successfully',
+        message: 'No clients existing',
+      });
+    } else {
+      res.status(200).json({
+        status: 'Successfully',
+        count: Clients.length,
+        data: Clients,
+      });
+    }
   } catch (error) {
-    res.status(400).send({ status: 'failed', message: error.message });
+    res.status(400).send(error);
   }
 };
 
