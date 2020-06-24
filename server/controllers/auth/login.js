3f460b8ae99b7d6c6ecb5533ed212b899fa226a5
Ali <ali.dahdouh@gmail.com>
2020-06-24 21:29:31 +0300

    fixed admin token naming relates #15

--- server/controllers/auth/login.js
+++ server/controllers/auth/login.js
@@ -45,7 +45,7 @@ const login = async (req, res) => {
             res.status(400).json(errResponse);
           }
 
-          const adminToken = { userId: admins.id };
+          const adminToken = { adminId: admins.id };
           const cookie = sign(adminToken, process.env.SECRET_KEY);
           res.cookie('admin', cookie).json({
             status: 'successfully',
