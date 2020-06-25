1202be8b4e95f3ae4ed3b90bb7c40a934d6733aa
ahmadysalah <a.ahmad.salah@gmail.com>
2020-06-25 10:33:44 +0300

    push add bank test #68

--- __test__/routes/addBanks.test.js
+++ __test__/routes/addBanks.test.js
@@ -7,7 +7,7 @@ const request = superTest(app);
 const dbConnection = require('../../server/database/dbConnection');
 const buildDB = require('../../server/database/index');
 
-describe('request to add, delete /bank', () => {
+describe('request to add /bank', () => {
   beforeAll(() => buildDB);
   afterAll(() => dbConnection.close());
 
@@ -83,61 +83,6 @@ describe('request to add, delete /bank', () => {
           .set('cookie', res.header['set-cookie']);
         expect(response.status).toBe(200);
         expect(response.body.message).toBe('User Bank added successfully');
-      });
-    done();
-  });
-
-  // delete bank Account
-  it('return status 401 for delete bank without auth', async (done) => {
-    const response = await request.delete('/api/v1/client/bank');
-    expect(response.status).toBe(401);
-    done();
-  });
-
-  it('return status 400 for delete bank account not exist', async (done) => {
-    const reqBody = {
-      fullName: 'احمد صلاح',
-      password: '*aA123456*',
-      passwordConfirmation: '*aA123456*',
-      mainBankName: 'بنك فلسطين',
-      mainBankAccount: 12368,
-      email: 'testdeletebank2@gmail.com',
-    };
-    request
-      .post('/api/v1/signup')
-      .send(reqBody)
-      .then(async (res) => {
-        const response = await request
-          .delete('/api/v1/client/bank')
-          .send({ accountNumber: '852147' })
-          .set('cookie', res.header['set-cookie']);
-        expect(response.status).toBe(400);
-      });
-    done();
-  });
-
-  it('return status 200 for delete bank account exist', async (done) => {
-    const reqBody = {
-      fullName: 'احمد صلاح',
-      password: '*aA123456*',
-      passwordConfirmation: '*aA123456*',
-      mainBankName: 'بنك فلسطين',
-      mainBankAccount: 963258,
-      email: 'testdeletebank3@gmail.com',
-    };
-    request
-      .post('/api/v1/signup')
-      .send(reqBody)
-      .then(async (res) => {
-        await request
-          .post('/api/v1/client/bank')
-          .send({ accountNumber: 63214 })
-          .set('cookie', res.header['set-cookie']);
-        const response = await request
-          .delete('/api/v1/client/bank')
-          .send({ accountNumber: 63214 })
-          .set('cookie', res.header['set-cookie']);
-        expect(response.status).toBe(200);
       });
     done();
   });
