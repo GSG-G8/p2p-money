cfa22583abd7476d6560ead34c9d2a074ca9f491
mFlaifel <msf-2oo7@hotmail.com>
2020-06-25 15:41:22 +0300

    update client data on the database relates #22

--- server/controllers/routes/client/createTransaction.js
+++ server/controllers/routes/client/createTransaction.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable camelcase */
 const transactions = require('../../../database/models/transactions');
 const exchangeMoney = require('../../../database/models/exchangeMoney');
@@ -19,13 +20,14 @@ const createTransaction = async (req, res) => {
     const agencyPrice = latestPrices[0].tellerPrice.filter(
       (e) => e.from === from && e.To === to
     );
-    console.log(appPrice[0].sell);
-    const clientData = await client.find({ _id: res.clientId });
+    // console.log(appPrice[0].sell);
+    const clientData = await client.findById(res.clientId);
+    const clientBalance = clientData.mainBalance;
     // get the client currency balance which he wants to convert from
-    const clientBalanceToConvertFromArray = clientData[0].mainBalance.filter(
+    const clientBalanceToConvertFromArray = clientBalance.filter(
       (element) => element.type === from
     );
-    const clientBalanceToConvertToArray = clientData[0].mainBalance.filter(
+    const clientBalanceToConvertToArray = clientBalance.filter(
       (element) => element.type === to
     );
     // check if there is enough balance to complete the transaction
@@ -43,42 +45,39 @@ const createTransaction = async (req, res) => {
           to,
           app_price_sell: appPrice[0].sell * amount, // this will added to client
           app_price_buy: appPrice[0].buy * amount,
-          app_saved_money: appPrice[0].buy * amount - bankPrice[0].buy * amount, // need review
+          app_saved_money:
+            appPrice[0].sell * amount - bankPrice[0].sell * amount, // need review
           spread: appPrice[0].buy * amount - appPrice[0].sell * amount, // need review
           bank_price_sell: bankPrice[0].sell * amount,
           bank_price_buy: bankPrice[0].buy * amount,
           agency_price_sell: agencyPrice[0].sell * amount,
           agency_price_buy: agencyPrice[0].buy * amount,
           agency_saved_money:
-            agencyPrice[0].buy * amount - bankPrice[0].buy * amount, // need review
+            agencyPrice[0].sell * amount - bankPrice[0].sell * amount, // need review
         })
-        .then((data) =>
-          transactions.create({ client_id, exchange_id: data._id })
-        )
-        .then(() => {
-          if (clientBalanceToConvertToArray === 0) {
-            client.findByIdAndUpdate(client_id, {});
+        .then((data) => {
+          transactions.create({ client_id, exchange_id: data._id });
+        })
+        .then(async () => {
+          clientBalance.forEach((e) => {
+            if (e.type === from) {
+              e.total -= amount;
+            }
+            if (e.type === to) {
+              e.total += appPrice[0].sell;
+            }
+          });
+          if (clientBalanceToConvertToArray.length === 0) {
+            clientBalance.push({ type: to, total: appPrice[0].sell });
+            console.log(clientBalance, appPrice[0].sell);
           }
-        });
+          await client.updateOne(
+            { _id: client_id },
+            { mainBalance: clientBalance }
+          );
+        })
+        .then(() => res.status(200).json({ message: 'Transaction Completed' }));
     }
-    // console.log(latestPrices[0].appPrice);
-    res.send({ latestPrices });
-    // await prices.find({
-    //   client_id: res.clientId,
-    // });
-    // let promise = Promise.resolve();
-    // const result = [];
-    // const exchangeDetail = [];
-    // const clientData = await client.findById(res.clientId);
-    // if (clientData.length === 0) throw new Error();
-    // result.push({ clientData });
-    // transactionTableData.forEach(({ exchange_id }) => {
-    //   promise = exchangeMoney.findById(exchange_id).then((exchangeData) => {
-    //     exchangeDetail.push(exchangeData);
-    //   });
-    // });
-    // result.push({ exchangeDetail });
-    // promise.then(() => res.status(200).json({ result }));
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
