ff394206d362a1ccc529bdb514abfe47b3714960
Ali <ali.dahdouh@gmail.com>
2020-06-25 11:13:45 +0300

    fixed test to back

--- __test__/routes/index.test.js
+++ __test__/routes/index.test.js
@@ -3,8 +3,10 @@ const loginTests = require('./login');
 const logoutTests = require('./logout');
 
 const dbConnection = require('../../server/database/dbConnection');
+// const buildDB = require('../../server/database/index');
 
 const runTests = async () => {
+  // beforeAll(() => buildDB);
   afterAll(() => dbConnection.close());
 
   // await signupTests();
