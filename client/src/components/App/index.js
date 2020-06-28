cf3bc8344574c9128b3a1eb535c3d3b88e8962a6
ha77an <ha77an19@gmail.com>
2020-06-28 20:14:23 +0300

    delete Typography from App

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import './style.css';
-import Ty from '../Common/Typography';
 
 function App() {
-  return (
-    <div>
-      <Ty />
-    </div>
-  );
+  return <div>Welcome in P2P Exchange Money App</div>;
 }
 
 export default App;
