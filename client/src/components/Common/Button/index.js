8cb67958098a613848c25db2fe682ba2f85d19b1
mFlaifel <msf-2oo7@hotmail.com>
2020-06-28 20:58:11 +0300

    antDesign Button relates #94

---
+++ client/src/components/Common/Button/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, ConfigProvider } from 'antd';
+import 'antd/dist/antd.css';
+
+const ButtonComponent = ({
+  Content,
+  disabled,
+  loading,
+  size,
+  block,
+  onClick,
+}) => (
+  <ConfigProvider direction="rtl">
+    <Button
+      type="primary"
+      disabled={disabled}
+      loading={loading}
+      size={size}
+      block={block}
+      onClick={onClick}
+    >
+      {Content}
+    </Button>
+  </ConfigProvider>
+);
+ButtonComponent.propTypes = {
+  Content: PropTypes.string,
+  disabled: PropTypes.bool,
+  loading: PropTypes.bool,
+  size: PropTypes.string,
+  block: PropTypes.bool,
+  onClick: PropTypes.func,
+};
+
+ButtonComponent.defaultProps = {
+  Content: 'تسجيل الدخول',
+  disabled: false,
+  loading: false,
+  size: 'all', // large,middle,sm
+  block: false, // make the button block element with full div width
+  onClick: null,
+};
+
+export default ButtonComponent;
