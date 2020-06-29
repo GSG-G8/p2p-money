33faae5592af9fae3ab3a6c5ac1648018500297e
Ali <ali.dahdouh@gmail.com>
2020-06-29 09:50:27 +0300

    fixed sider responsive relates #88

--- client/src/components/client/sider/index.js
+++ client/src/components/client/sider/index.js
@@ -1,52 +1,47 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import './index.css';
-import { Layout, Menu } from 'antd';
-import {
-  UserOutlined,
-  UploadOutlined,
-  VideoCameraOutlined,
-} from '@ant-design/icons';
-import bank from '../../../assets/icons';
+import { Layout, Menu, ConfigProvider } from 'antd';
+import { WalletFilled, BankFilled, SettingFilled } from '@ant-design/icons';
 
 const { Content, Sider } = Layout;
 const SiderMenu = () => (
-  <div className="sider-wrapper">
-    <Layout>
-      <Sider
-        style={{
-          overflow: 'auto',
-          height: '100vh',
-          position: 'fixed',
-          left: 0,
-        }}
-      >
-        <Menu theme="light" mode="inline" defaultSelectedKeys={['4']}>
-          <Menu.Item key="1" icon={<UserOutlined />}>
-            nav 1
-          </Menu.Item>
-          <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-            nav 2
-          </Menu.Item>
-          <Menu.Item key="3" icon={<UploadOutlined />}>
-            nav 3
-          </Menu.Item>
-        </Menu>
-      </Sider>
-      <Layout className="site-layout" style={{ marginLeft: 200 }}>
-        <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
-          <div
-            className="site-layout-background"
-            style={{ padding: 24, textAlign: 'center' }}
+  <ConfigProvider direction="rtl">
+    <div className="sider-wrapper">
+      <Layout dir="rtl">
+        <Sider dir="rtl" className="sider-rtl">
+          <Menu
+            theme="light"
+            className="side-menu"
+            mode="inline"
+            defaultSelectedKeys={['4']}
           >
-            ...
-            <br />
-            content
-          </div>
-        </Content>
+            <Menu.Item key="1" icon={<WalletFilled />}>
+              المحفظة
+            </Menu.Item>
+            <Menu.Item key="2" icon={<BankFilled />}>
+              حساب البنك
+            </Menu.Item>
+            <Menu.Item key="3" icon={<SettingFilled />}>
+              إعدادت
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+            <div
+              className="site-layout-background"
+              style={{ padding: 24, textAlign: 'center' }}
+            >
+              ...
+              <br />
+              content
+            </div>
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
-  </div>
+    </div>
+  </ConfigProvider>
 );
 
 export default SiderMenu;
