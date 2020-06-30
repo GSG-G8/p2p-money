c684af2ca313ab047f0ec163ca201ec023d8783d
ha77an <ha77an19@gmail.com>
2020-06-30 14:07:14 +0300

    added arabic font family

--- client/src/components/Common/Typography/index.js
+++ client/src/components/Common/Typography/index.js
@@ -10,12 +10,14 @@ const { Title, Paragraph } = Typography;
 
 const typography = ({ type, level, Content }) => (
   <ConfigProvider direction="rtl">
-    {type === 'title' ? (
-      // this Title have 4 level h1:1,  h2:2, h3:3, h4:4
-      <Title level={level}>{Content}</Title>
-    ) : (
-      <Paragraph>{Content}</Paragraph>
-    )}
+    <div className="Typography">
+      {type === 'title' ? (
+        // this Title have 4 level h1:1,  h2:2, h3:3, h4:4
+        <Title level={level}>{Content}</Title>
+      ) : (
+        <Paragraph>{Content}</Paragraph>
+      )}
+    </div>
   </ConfigProvider>
 );
 
@@ -24,11 +26,10 @@ export default typography;
 typography.propTypes = {
   type: PropTypes.string,
   level: PropTypes.number,
-  Content: PropTypes.string,
+  Content: PropTypes.string.isRequired,
 };
 
 typography.defaultProps = {
-  type: 'title',
+  type: '',
   level: 1,
-  Content: 'مرحباً',
 };
