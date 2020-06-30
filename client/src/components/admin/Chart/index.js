c5225496fd71ea121ac9a9ba6b573fc44892e774
ha77an <ha77an19@gmail.com>
2020-06-30 11:10:56 +0300

    added proptypes for chat

--- client/src/components/admin/Chart/index.js
+++ client/src/components/admin/Chart/index.js
@@ -1,80 +1,89 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
 import { MDBContainer } from 'mdbreact';
+import PropTypes from 'prop-types';
+
 import './style.css';
 
-const dataLine = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-  datasets: [
-    {
-      label: 'My First dataset',
-      fill: true,
-      lineTension: 0.3,
-      backgroundColor: 'rgba(225, 204,230, .3)',
-      borderColor: 'rgb(205, 130, 158)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgb(205, 130,1 58)',
-      pointBackgroundColor: 'rgb(255, 255, 255)',
-      pointBorderWidth: 10,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgb(0, 0, 0)',
-      pointHoverBorderColor: 'rgba(220, 220, 220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [65, 59, 80, 81, 56, 55, 40],
-    },
-    {
-      label: 'My Second dataset',
-      fill: true,
-      lineTension: 0.3,
-      backgroundColor: 'rgba(184, 185, 210, .3)',
-      borderColor: 'rgb(35, 26, 136)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgb(35, 26, 136)',
-      pointBackgroundColor: 'rgb(255, 255, 255)',
-      pointBorderWidth: 10,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgb(0, 0, 0)',
-      pointHoverBorderColor: 'rgba(220, 220, 220, 1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [28, 48, 40, 19, 86, 27, 300],
-    },
-    {
-      label: 'My Third dataset',
-      fill: true,
-      lineTension: 0.3,
-      backgroundColor: 'rgba(184, 185, 210, .3)',
-      borderColor: 'rgb(35, 12,79)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgb(35, 26, 012)',
-      pointBackgroundColor: 'rgb(255, 255, 255)',
-      pointBorderWidth: 15,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgb(0, 0, 0)',
-      pointHoverBorderColor: 'rgba(220, 220, 220, 1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [8, 19, 40, 5, 20, 27, 0],
-    },
-  ],
+const ChartsPage = ({ routes, data }) => {
+  const dataLine = {
+    labels: [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ],
+    datasets: [
+      {
+        label: routes[0],
+        fill: true,
+        lineTension: 0.3,
+        backgroundColor: 'rgba(225, 204,230, .3)',
+        borderColor: 'rgb(205,20,15)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgb(205,20,15)',
+        pointBackgroundColor: 'rgb(205,20,15)',
+        pointBorderWidth: 10,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgb(0, 0, 0)',
+        pointHoverBorderColor: 'rgba(220, 220, 220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: data[0],
+      },
+      {
+        label: routes[1],
+        fill: true,
+        lineTension: 0.3,
+        backgroundColor: 'rgba(184, 185, 210, .3)',
+        borderColor: 'rgb(35, 26, 136)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgb(35, 26, 136)',
+        pointBackgroundColor: 'rgb(35, 26, 136)',
+        pointBorderWidth: 10,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgb(0, 0, 0)',
+        pointHoverBorderColor: 'rgba(220, 220, 220, 1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: data[1],
+      },
+    ],
+  };
+  return (
+    <MDBContainer className="Chart">
+      <Line data={dataLine} options={{ responsive: true }} />
+    </MDBContainer>
+  );
 };
-const ChartsPage = () => (
-  <MDBContainer className="Chart">
-    <Line data={dataLine} options={{ responsive: true }} />
-  </MDBContainer>
-);
 
 export default ChartsPage;
+
+ChartsPage.prototype = {
+  routes: PropTypes.arrayOf(PropTypes.array),
+  data: PropTypes.arrayOf(PropTypes.string),
+};
+
+ChartsPage.defaultProps = {
+  routes: ['Home', 'Wallet'],
+  data: [
+    [1, 2, 5, 7, 30, 15, 12, 3, 65, 12, 10, 3],
+    [55, 79, 3, 66, 7, 0, 1, 30, 92, 27, 63, 71],
+  ],
+};
