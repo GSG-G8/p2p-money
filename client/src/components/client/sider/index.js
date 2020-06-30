301f17f3f480e8f681c86f9deac106b66804c9d9
Ali <ali.dahdouh@gmail.com>
2020-06-30 16:48:30 +0300

    added user logo and title by props relates #88

--- client/src/components/client/sider/index.js
+++ client/src/components/client/sider/index.js
@@ -1,43 +1,57 @@
 import React from 'react';
 import { Layout, Menu, ConfigProvider } from 'antd';
+import PropTypes from 'prop-types';
 import { WalletFilled, BankFilled, SettingFilled } from '@ant-design/icons';
-import logo from '../../../assets/icons/icon.svg';
+import { Link } from 'react-router-dom';
+import Typography from '../../Common/Typography';
 
 import 'antd/dist/antd.css';
 import './style.css';
 
 const { Content, Sider } = Layout;
-const SiderMenu = () => (
+const SiderMenu = ({ content, avatar, userTitle }) => (
   <ConfigProvider direction="rtl">
     <div className="sider-wrapper">
       <Layout dir="rtl">
         <Sider dir="rtl" className="sider-rtl">
-          <div className="sider-logo">
-            <img src={logo} alt="logo" /> P2P Money
+          <div className="user-wrapper">
+            <img className="sider-logo" src={avatar} alt="user-name" />
+            <Typography className="user-title" Content={userTitle} />
           </div>
-          <Menu theme="light" mode="inline" defaultSelectedKeys={['4']}>
-            <Menu.Item key="1" icon={<WalletFilled />}>
+          <Menu
+            theme="light"
+            mode="inline"
+            defaultSelectedKeys={window.location.pathname.split('/')[1]}
+          >
+            <Menu.Item key="wallet" icon={<WalletFilled />}>
+              <Link to="/wallet" />
               محفظتي
             </Menu.Item>
-            <Menu.Item key="2" icon={<BankFilled />}>
+            <Menu.Item key="bank" icon={<BankFilled />}>
+              <Link to="/bank" />
               تفاصيل البنك
             </Menu.Item>
-            <Menu.Item key="3" icon={<SettingFilled />}>
+            <Menu.Item key="settings" icon={<SettingFilled />}>
+              <Link to="/settings" />
               الإعدادات
             </Menu.Item>
           </Menu>
         </Sider>
         <Layout className="wrap-layout">
           <Content>
-            <div className="wrap-layout__content">
-              <br />
-              content
-            </div>
+            <div className="wrap-layout__content" />
+            {content}
           </Content>
         </Layout>
       </Layout>
     </div>
   </ConfigProvider>
 );
+
+SiderMenu.propTypes = {
+  content: PropTypes.element.isRequired,
+  avatar: PropTypes.string.isRequired,
+  userTitle: PropTypes.string.isRequired,
+};
 
 export default SiderMenu;
