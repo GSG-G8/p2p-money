5d340bb8d1f413eb202f84ab4ed9a160441b8b31
mFlaifel <msf-2oo7@hotmail.com>
2020-07-01 12:40:14 +0300

    update buton component update header style add new font the project for the header relates #123

--- client/src/components/Common/Header/index.js
+++ client/src/components/Common/Header/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { ConfigProvider, Button } from 'antd';
+import { ConfigProvider } from 'antd';
 
 import headerIcon from '../../../assets/icons/headerIcon.svg';
-
-import 'antd/dist/antd.css';
+import Button from '../Button';
+// import 'antd/dist/antd.css';
 import './style.css';
 
 const Header = () => (
@@ -11,15 +11,15 @@ const Header = () => (
     <header className="header-component">
       <div className="header-right flex-row">
         <img src={headerIcon} className="header__img" alt="site logo" />
-        <p className="text--white--almarai  logo-text">P2P</p>
+        <p className=" logo-text">P2P</p>
       </div>
       <div className="header-left flex-row">
-        <Button className="header-btn btn-login" type="text">
-          <p className="text--white">تسجيل الدخول</p>
-        </Button>
-        <Button className="header-btn btn-signup" type="text">
-          <p className="text--white">حساب جديد</p>
-        </Button>
+        <Button className="header-btn btn-login" />
+        <Button
+          cssClass="blue-border"
+          className="header-btn btn-signup"
+          content="حساب جديد"
+        />
       </div>
     </header>
   </ConfigProvider>
