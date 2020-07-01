561eaacc68464d67c56a0ebebad8adf53396dccf
mFlaifel <msf-2oo7@hotmail.com>
2020-07-01 16:21:03 +0300

    update button to change according to user status if signin or not relates #123

--- client/src/components/Common/Header/index.js
+++ client/src/components/Common/Header/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { ConfigProvider } from 'antd';
+import axios from 'axios';
+import Cookies from 'js-cookie';
 
 import headerIcon from '../../../assets/icons/headerIcon.svg';
 import Button from '../Button';
@@ -13,24 +15,37 @@ const Header = () => {
       <header className="header-component">
         <div className="header-right flex-row">
           <img src={headerIcon} className="header__img" alt="site logo" />
-          <p className=" logo-text">P2P</p>
+          <p className=" logo-text">P2P Money</p>
         </div>
         <div className="header-left flex-row">
-          <Button
-            className="header-btn btn-login"
-            content="تسجيل الدخول"
-            onClick={() => {
-              history.push('/login');
-            }}
-          />
-          <Button
-            cssClass="blue-border"
-            className="header-btn btn-signup"
-            content="حساب جديد"
-            onClick={() => {
-              history.push('/signup');
-            }}
-          />
+          {Cookies.get('client') || Cookies.get('admin') ? (
+            <Button
+              cssClass="blue-border"
+              className="header-btn btn-signup"
+              content="خروج"
+              onClick={async () => {
+                await axios.post('/api/v1/logout');
+              }}
+            />
+          ) : (
+            <>
+              <Button
+                className="header-btn btn-login"
+                content="تسجيل الدخول"
+                onClick={() => {
+                  history.push('/login');
+                }}
+              />
+              <Button
+                cssClass="blue-border"
+                className="header-btn btn-signup"
+                content="حساب جديد"
+                onClick={() => {
+                  history.push('/signup');
+                }}
+              />
+            </>
+          )}
         </div>
       </header>
     </ConfigProvider>
