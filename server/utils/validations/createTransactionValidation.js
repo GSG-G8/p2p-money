0674dc8b98454e5f2cc37e2b0a513001c1ec70ad
mFlaifel <msf-2oo7@hotmail.com>
2020-07-01 10:15:26 +0300

    update validion to include EUR relates #22

--- server/utils/validations/createTransactionValidation.js
+++ server/utils/validations/createTransactionValidation.js
@@ -4,11 +4,11 @@ const schema = yup.object().shape({
   amount: yup.number().required(),
   from: yup
     .mixed()
-    .oneOf(['USD', 'ILS', 'JOD', 'EGP'])
+    .oneOf(['USD', 'ILS', 'JOD', 'EGP', 'EUR'])
     .required('Please choose the currency from type.'),
   to: yup
     .mixed()
-    .oneOf(['USD', 'ILS', 'JOD', 'EGP'])
+    .oneOf(['USD', 'ILS', 'JOD', 'EGP', 'EUR'])
     .required('Please choose the currency to type.'),
 });
 
