f6af994ffa0aecd39d3778038f227d82806db66f
ahmadysalah <a.ahmad.salah@gmail.com>
2020-07-07 13:41:53 +0300

    Dashboard Admin Page Layout - create dashboard Page -Relate #43

--- client/src/components/pages/admin/dashboard/index.js
+++ client/src/components/pages/admin/dashboard/index.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
+import CardBox from '../../../admin/AdminCard';
+import Charts from '../../../admin/Chart';
+import ClientCard from '../../../admin/clientsCard';
 
 import './style.css';
 
-const dashboard = () => <>dashboard</>;
+const Dashboard = ({ Clients, Transactions }) => {
+  const [LogMobile, setMobile] = useState(true);
 
-export default dashboard;
+  return (
+    <div className="dashboard__page">
+      <Helmet>
+        <title>لوحة التحكم</title>
+      </Helmet>
+      <div className="dashboard__head__card">
+        <CardBox type="transaction" title="بسيبسيب" description="بسيب" />
+        <CardBox type="clients" title="بسي" description="سبيب" />
+        <CardBox title=""   title="تعــديل" description="الاســعار" />
+      </div>
+      {/* <Charts /> */}
+    </div>
+  );
+};
+
+Dashboard.propTypes = {
+  Clients: PropTypes.arrayOf(PropTypes.object).isRequired,
+  Transactions: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default Dashboard;
