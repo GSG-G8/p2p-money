683d868672c56cee356792c1b148d2d0340d4c25
ahmadysalah <a.ahmad.salah@gmail.com>
2020-07-08 20:04:03 +0300

    create function to update values and change the state #142

--- client/src/components/pages/admin/dashboard/updatePrice.js
+++ client/src/components/pages/admin/dashboard/updatePrice.js
@@ -9,32 +9,41 @@ import './style.css';
 
 const getPrices = async () => {
   const { data: Prices } = await axios.get('/api/v1/prices');
-  console.log(Prices.Prices);
   return Prices.Prices;
 };
-
-const getPriceData = (array, from, to, cases) =>
-  array
-    .map((element) =>
-      element.from === from && element.to === to
-        ? Number(element[cases]).toFixed(3)
-        : 0
-    )
-    .filter((e) => e !== 0);
 
 const UpdatePriceForm = ({ visible, handleHide }) => {
   const [exchangePrice, setPrices] = useState(0);
   const [alert, setAlert] = useState();
   const [From, setFrom] = useState('USD');
   const [To, setTo] = useState('ILS');
-  console.log(exchangePrice);
   const {
     appPrice,
     tellerPrice,
     bankPrice,
-    lastUpdate,
     screenPrice,
+    lastUpdate,
   } = exchangePrice;
+
+  const getPriceData = (array, from, to, cases) =>
+    array
+      .map((element) =>
+        element.from === from && element.to === to
+          ? Number(element[cases]).toFixed(3)
+          : 0
+      )
+      .filter((e) => e !== 0);
+
+  const handleChange = (array, from, to, cases, value) => {
+    exchangePrice[array].map((price, index) => {
+      if (price.from === from && price.to === to) {
+        exchangePrice[array][index][cases] = value;
+        return 0;
+      }
+    });
+    setPrices({ [array]: exchangePrice[array], ...exchangePrice });
+  };
+
   useEffect(() => {
     if (!exchangePrice)
       getPrices()
@@ -89,33 +98,55 @@ const UpdatePriceForm = ({ visible, handleHide }) => {
           </div>
           <div className="update__content">
             <InputText
-              // type="number"
-              // handleChange={(e)=>setBuyBank()}
+              type="number"
+              handleChange={({ target: { value } }) =>
+                handleChange('bankPrice', From, To, 'buy', value)
+              }
               value={getPriceData(bankPrice, From, To, 'buy')}
               className="update__input"
             />
             <InputText
+              type="number"
               className="update__input"
               value={getPriceData(tellerPrice, From, To, 'buy')}
+              handleChange={({ target: { value } }) =>
+                handleChange('tellerPrice', From, To, 'buy', value)
+              }
             />
             <InputText
+              type="number"
               className="update__input"
               value={getPriceData(appPrice, From, To, 'buy')}
+              handleChange={({ target: { value } }) =>
+                handleChange('appPrice', From, To, 'buy', value)
+              }
             />
             <Text className="update__text" Content="سعر الشراء" />
           </div>
           <div className="update__content">
             <InputText
+              type="number"
               className="update__input"
               value={getPriceData(bankPrice, From, To, 'sell')}
+              handleChange={({ target: { value } }) =>
+                handleChange('bankPrice', From, To, 'sell', value)
+              }
             />
             <InputText
+              type="number"
               className="update__input"
               value={getPriceData(tellerPrice, From, To, 'sell')}
+              handleChange={({ target: { value } }) =>
+                handleChange('tellerPrice', From, To, 'sell', value)
+              }
             />
             <InputText
+              type="number"
               className="update__input"
               value={getPriceData(appPrice, From, To, 'sell')}
+              handleChange={({ target: { value } }) =>
+                handleChange('appPrice', From, To, 'sell', value)
+              }
             />
             <Text className="update__text" Content="سعـر البيـــع" />
           </div>
